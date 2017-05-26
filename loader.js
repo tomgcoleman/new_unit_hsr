683f7ffa8aa48828e707362974ca7e7a69767a50
Tom Coleman <tom.g.coleman@gmail.com>
2017-05-26 10:41:37 -0700

    initial child file loading

--- loader.js
+++ loader.js
@@ -1,7 +1,7 @@
 console.log('injecting code from https://ewegithub.sb.karmalab.net/EWE/new_unit_hsr');
 
 var filenames = [
-    'suites/hsr_test_suites.js'
+    'suites/hsr-test-suites.js'
 ];
 
 for (var i = 0 ; i < filenames.length ; i++) {    
