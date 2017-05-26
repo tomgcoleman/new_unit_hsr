d7cfe4ecaab6400fe570fa270308f311343bf187
Tom Coleman <tom.g.coleman@gmail.com>
2017-05-26 10:32:47 -0700

    load first child file

--- loader.js
+++ loader.js
@@ -3,3 +3,9 @@ console.log('injecting code from https://ewegithub.sb.karmalab.net/EWE/new_unit_
 var filenames = [
     'suites/hsr_test_suites.js'
 ];
+
+for (var i = 0 ; i < filenames.length ; i++) {    
+    var script_el = document.createElement('script');
+    script_el.src = "https://rawgit.com/tomgcoleman/new_unit_hsr/master/" + filenames[i];
+    document.getElementsByTagName('head')[0].appendChild(script_el)
+}
